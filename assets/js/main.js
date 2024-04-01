85bb331ce920ded409c27e2a9b246833dc1a2c69
Sabuhi0 <sabuhiq0@gmail.com>
2024-04-01 09:49:57 +0400

    Update  main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,31 @@
-console.log("Hello World");
+// console.log("Hello World");
+const cardItems = document.querySelectorAll('#card-item');
+let isCardItemSelected = false;
+let selectedUserInfo = null;
 
+cardItems.forEach((item) => {
+  item.addEventListener('click', function () {
+    isCardItemSelected = true;
+    selectedUserInfo = {
+      name: this.getAttribute('data-name'),
+      surname: this.getAttribute('data-surname'),
+      email: this.getAttribute('data-email'),
+    }
+    console.log(selectedUserInfo);
+  })
+})
+
+
+const btn = document.getElementById("btn");
+const warning_text = document.getElementById('alert-box');
+
+btn.addEventListener('click', function () {
+  if (!isCardItemSelected) {
+    warning_text.classList.add("show");
+    setTimeout(function () {
+      warning_text.classList.remove("show");
+    }, 1800)
+  } else {
+    console.log("Seçilen kullanici bilgileri:", selectedUserInfo);
+  }
+})
